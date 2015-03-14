1c508fe1dbaf4bdbe5cb6810c150645624427e62
David Ok <davidok@gmail.com>
2015-03-13 20:24:08 -0400

    Committing data file in an attempt to resolve Terminal issue.

--- data.js
+++
@@ -1,5 +0,0 @@
-ebolaData = [{
-        ["August 2014", 3052],   ["September 2014", 7157],  ["October 2014", 13567],
-        ["November 2014", 15935],  ["December 2014", 20206],  ["January 2015", 22092],
-        ["February 2015", 23729]
-       }];

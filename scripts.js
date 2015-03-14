da69380d4b8269045dd303d896260f271a31049d
David Ok <davidok@gmail.com>
2015-03-13 20:53:37 -0400

    Added third column and row as well as an annotation noting that February was the last update to data.

--- scripts.js
+++ scripts.js
@@ -8,11 +8,12 @@ function drawChart() {
 	var data = new google.visualization.DataTable();
 	data.addColumn('string', 'Month');
 	data.addColumn('number', 'Cases');
+	data.addColumn({type:'string', role:'annotation'}); // annotationText col.
 
 	data.addRows([
-	["August 2014", 3052],   ["September 2014", 7157],  ["October 2014", 13567],
-	["November 2014", 15935],  ["December 2014", 20206],  ["January 2015", 22092],
-	["February 2015", 23729]
+	["August 2014", 3052, ""],   ["September 2014", 7157, ""],  ["October 2014", 13567, ""],
+	["November 2014", 15935, ""],  ["December 2014", 20206, ""],  ["January 2015", 22092, ""],
+	["February 2015", 23729, "Last Available Data"]
 	]);
 
 	var options = {
